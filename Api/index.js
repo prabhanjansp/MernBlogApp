d0de22d57deb3b51e74ad91a43ffb88dcecff672
prabhanjansp <puranikchetan97@gmail.com>
2024-11-11 23:33:30 +0530

    mongo db establishment is done

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,17 @@
 import e from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("DB Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = e();
 app.listen(3000, () => {
   console.log("server is running on the port 3000");
